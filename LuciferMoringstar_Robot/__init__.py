63246cbd72a847fbf74e10e7e96bdc4958dffc01
Jikku333 <90546342+Jikku333@users.noreply.github.com>
2021-10-18 14:49:41 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,13 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  Jikku Filter
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Jikku](https://t.me/RHYTHMHACKER)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Join our channel : [Click Me](https://t.me/RHYTHMHACKER)
 """
