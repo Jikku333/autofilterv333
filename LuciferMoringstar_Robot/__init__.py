1692dd38b0f2006217d818915d46e3e702d92ee2
Jikku333 <90546342+Jikku333@users.noreply.github.com>
2021-10-18 15:48:43 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -10,20 +10,7 @@ from .Channel import (
    GENRES
 )
 
-HELP = """
-Basic Commads
-
-/start :- check am Alive
-
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
+HELP = """❌ No One Can Help You ❌
 
 """
 
